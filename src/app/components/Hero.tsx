0f26d4567ca16d0682ed49f481da6f63dc30148d
hamza lachgar <121760220+hamzalach02@users.noreply.github.com>
2024-09-17 20:41:03 +0100

    Update Hero.tsx

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -11,7 +11,7 @@ const Hero = () => {
           
           <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white sm:text-5xl md:text-6xl">
             <span className="block">Hi, I'm Hamza</span>
-            <span className="block text-indigo-600 dark:text-indigo-400">Software Developer</span>
+            <span className="block text-indigo-600 dark:text-indigo-400">Software Engineer</span>
           </h1>
           <p className="mt-3 max-w-md mx-auto text-base text-gray-500 dark:text-gray-300 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
             Passionate about building robust and scalable applications. Experienced in Java, JavaScript, Python, and more.
